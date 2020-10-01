d5c49d34cc2e3e906fb0636d2de28a437c9d8b88
Stephen Emery <stephen.emery@streem.pro>
2020-10-01 16:51:26 -0700

    Add test meshes.

--- Android/app/src/main/java/com/mehequanna/mmiw/MmiwActivity.kt
+++ Android/app/src/main/java/com/mehequanna/mmiw/MmiwActivity.kt
@@ -17,8 +17,8 @@ import androidx.core.app.ShareCompat
 import androidx.core.content.FileProvider.getUriForFile
 import com.google.ar.core.ArCoreApk
 import com.google.ar.core.AugmentedFace
-import com.google.ar.core.TrackingState
 import com.google.ar.sceneform.ArSceneView
+import com.google.ar.sceneform.rendering.ModelRenderable
 import com.google.ar.sceneform.rendering.Renderable
 import com.google.ar.sceneform.rendering.Texture
 import com.google.ar.sceneform.ux.AugmentedFaceNode
@@ -31,7 +31,8 @@ import java.io.IOException
 class MmiwActivity : AppCompatActivity() {
     private lateinit var arFragment: FaceArFragment
     private var faceMeshTexture: Texture? = null
-    private var faceNodeMap = HashMap<AugmentedFace, AugmentedFaceNode>()
+    var faceNodeMap = HashMap<AugmentedFace, AugmentedFaceNode>()
+    private var faceRegionsRenderable: ModelRenderable? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,41 +42,68 @@ class MmiwActivity : AppCompatActivity() {
 
         setContentView(R.layout.activity_mmiw_ar)
         arFragment = face_fragment as FaceArFragment
-        Texture.builder()
-            .setSource(this, R.drawable.red_hand_80_texture)
+
+        ModelRenderable.builder()
+            .setSource(this, Uri.parse("test.sfb"))
             .build()
-            .thenAccept { texture -> faceMeshTexture = texture }
+            .thenAccept { modelRenderable ->
+                faceRegionsRenderable = modelRenderable
+                modelRenderable.isShadowCaster = false
+                modelRenderable.isShadowReceiver = false
+            }
+
+
+//        Texture.builder()
+//            .setSource(this, R.drawable.red_hand_80_texture)
+//            .build()
+//            .thenAccept { texture -> faceMeshTexture = texture }
 
         val sceneView = arFragment.arSceneView
         sceneView.cameraStreamRenderPriority = Renderable.RENDER_PRIORITY_FIRST
         val scene = sceneView.scene
 
         scene.addOnUpdateListener {
-            faceMeshTexture.let {
+            faceRegionsRenderable?.let {
                 sceneView.session
                     ?.getAllTrackables(AugmentedFace::class.java)?.let {
                         for (f in it) {
                             if (!faceNodeMap.containsKey(f)) {
                                 val faceNode = AugmentedFaceNode(f)
                                 faceNode.setParent(scene)
-                                faceNode.faceMeshTexture = faceMeshTexture
-                                faceNodeMap[f] = faceNode
+                                faceNode.faceRegionsRenderable = faceRegionsRenderable
+                                faceNodeMap.put(f, faceNode)
                             }
                         }
-                        // Remove any AugmentedFaceNodes associated with an AugmentedFace that stopped tracking.
-                        val iter = faceNodeMap.entries.iterator()
-                        while (iter.hasNext()) {
-                            val entry = iter.next()
-                            val face = entry.key
-                            if (face.trackingState == TrackingState.STOPPED) {
-                                val faceNode = entry.value
-                                faceNode.setParent(null)
-                                iter.remove()
-                            }
-                        }
+                        // Remove any AugmentedFaceNodes associated with
+                        //an AugmentedFace that stopped tracking.
                     }
             }
         }
+
+//            faceMeshTexture.let {
+//                sceneView.session
+//                    ?.getAllTrackables(AugmentedFace::class.java)?.let {
+//                        for (f in it) {
+//                            if (!faceNodeMap.containsKey(f)) {
+//                                val faceNode = AugmentedFaceNode(f)
+//                                faceNode.setParent(scene)
+//                                faceNode.faceMeshTexture = faceMeshTexture
+//                                faceNodeMap.put(f, faceNode)
+//                            }
+//                        }
+//                        // Remove any AugmentedFaceNodes associated with an AugmentedFace that stopped tracking.
+//                        val iter = faceNodeMap.entries.iterator()
+//                        while (iter.hasNext()) {
+//                            val entry = iter.next()
+//                            val face = entry.key
+//                            if (face.trackingState == TrackingState.STOPPED) {
+//                                val faceNode = entry.value
+//                                faceNode.setParent(null)
+//                                iter.remove()
+//                            }
+//                        }
+//                    }
+//            }
 
         capture_button.setOnClickListener {
             capture_button.isEnabled = false
