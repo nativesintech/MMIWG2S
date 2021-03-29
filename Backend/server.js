70e0a238e53d66930dcfeda58772ae106f2a4b88
Stephen Emery <emery.stephen@gmail.com>
2021-03-28 21:41:55 -0700

    Just keep coding

--- Backend/server.js
+++ Backend/server.js
@@ -1,14 +1,24 @@
 console.log('May Node be with you');
 
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.listen(3000, function() {
-    console.log('listening on 3000')
+    console.log('listening on 3000');
 });
+
+// app.get('/', (request, response) => {
+//     response.send('Hello World')
+// });
 
 app.get('/', (request, response) => {
-    response.send('Hello World')
+  response.sendFile(__dirname + '/index.html');
 });
 
-
+app.post('/userinfo', (request, response) => {
+  console.log('Give me your userinfo, please.');
+  console.log(request.body);
+});
