c775cb0bcc451be35f3f3881e5944420d0bd0d19
root <pavan4@umbc.edu>
2022-05-04 14:32:16 +0000

    Route fixes

--- mmiwg2s-backend/routes/index.js
+++ mmiwg2s-backend/routes/index.js
@@ -3,13 +3,14 @@ var router = express.Router();
 const submissionController = require('../controllers/submissionController.js');
 
 
-// router.get('/', function(req, res, next) {
-//   // res.render('index', { title: 'Express' });
-//   res.render('index', {});
-// });
+router.get('/', function(req, res, next) {
+  // res.render('index', { title: 'Express' });
+  // res.render('index', {});
+  res.sendStatus(200);
+});
 
 /* GET home page. */
-router.get('/', submissionController.render);
+router.get('/shared-with-us', submissionController.render);
 
 router.get('/health-check', (req, res) => {
   res.send('ok');
