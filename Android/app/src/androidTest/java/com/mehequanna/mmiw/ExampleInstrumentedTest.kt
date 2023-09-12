68a968713b8fd62846c66ed947784fdd1aac6c7a
Stephen Emery <emery.stephen@gmail.com>
2023-09-11 20:37:42 -0700

    Update Android project to Android 33 for Play Store requirements.

--- Android/app/src/androidTest/java/com/mehequanna/mmiw/ExampleInstrumentedTest.kt
+++ Android/app/src/androidTest/java/com/mehequanna/mmiw/ExampleInstrumentedTest.kt
@@ -18,7 +18,7 @@ class ExampleInstrumentedTest {
     @Test
     fun useAppContext() {
         // Context of the app under test.
-        val appContext = InstrumentationRegistry.getTargetContext()
+        val appContext = InstrumentationRegistry.getInstrumentation().context
         assertEquals("com.mehequanna.mmiw", appContext.packageName)
     }
 }
